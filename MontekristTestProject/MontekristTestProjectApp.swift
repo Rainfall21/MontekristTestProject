dd43f5427eacd30a05759f2e65c9e0ddb02bee23
alibekismagulov <na.ismagulov@gmail.com>
2023-08-14 19:54:18 +0300

    Initial Commit

---
+++ MontekristTestProject/MontekristTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MontekristTestProjectApp.swift
+//  MontekristTestProject
+//
+//  Created by Alibek Ismagulov on 14.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MontekristTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
